6da3e92ebbcaea7b7d044d8056136b7320bdf579
speedingman <hcdo@andats.com>
2019-01-12 15:54:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/zeros/apipractice/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.zeros.apipractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+//        로그인 버튼이 눌리면 할일
+
+
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
